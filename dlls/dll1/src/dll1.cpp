9ed9688d0accdaea7bcbf8252ad546aebf904e8b
José Edmilson V. dos Santos Jr <ejunior@gmail.com>
2018-10-23 08:39:00 -0300

    teste novo for

--- dlls/dll1/src/dll1.cpp
+++ dlls/dll1/src/dll1.cpp
@@ -12,11 +12,17 @@ Dll1::~Dll1(){}
 const std::string Dll1::getText(){
 
 	Lib1 lib1;
-	
+
+
 	std::string a(ClasseA::value());
 	std::string b(lib1.getText());
 	
 	std::string valor = a+" "+b;
+
+	for( const auto &x: {1,2,3,5}){
+		valor = valor + "\n" + std::to_string(x);
+	}
+
 
 	return valor;
 }
