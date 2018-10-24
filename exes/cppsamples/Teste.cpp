f59afcaf2eb17cd0469fe0de2474d951c27a8911
José Edmilson V. dos Santos Jr <ejunior@gmail.com>
2018-10-23 22:14:13 -0300

    templates and overloads with rvalues

--- exes/cppsamples/Teste.cpp
+++ exes/cppsamples/Teste.cpp
@@ -5,18 +5,53 @@
 #include <boost/format.hpp>
 #include <utility>
 #include <iostream>
+
 using namespace std;
 using namespace boost;
 
-auto sum_product(double a, double b){
-    return make_tuple(a, b, a+b, a*b);
+auto sum_product(double a, double b) -> decltype(make_tuple(a, b, a + b, a * b)) { // -> not needed c++ 14
+    return make_tuple(a, b, a + b, a * b);
 }
 
-int main(){
-    auto x = sum_product(1.2,2);
+
+string getName() {
+    return "Junior";
+}
+
+void printName(const string &s) {
+    cout << "normal reference " << s << "\n";
+}
+
+void printName(string &&s) {
+    cout << "rvalue reference " << s << "\n";
+}
+
+template<typename T>
+void newPrintName(T t) {
+    cout << "now with template function " << t << "\n";
+}
+
+
+int main() {
+    auto x = sum_product(1.2, 2);
     auto y = make_pair(get<2>(x), get<3>(x));
 
-    std::cout << get<0>(x) << ", "<< get<1>(x);
+    cout << get<0>(x) << ", " << get<1>(x) <<", " << get<3>(x);  // ugly as hell
 
-    std::cout << format(" sum is %1% and multiply is %2%") % y.first % y.second << "\n";
+    cout << format(" sum is %1% and multiply is %2%") % y.first % y.second << "\n";
+
+    string &&nome = getName();
+
+    nome = "Edmilson";
+
+    cout << nome << " " << getName() << "\n";
+    cout << getName() << "\n\n";
+
+    printName(nome);
+    printName(getName());
+
+    newPrintName(nome);
+    newPrintName(getName());
+
+
 }
